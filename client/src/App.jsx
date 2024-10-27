eac50e46b3523229bb04f45089b9803e3f10e49e
matei <matei.dumitrescu@gmail.com>
2024-10-27 18:52:41 +0100

    initial page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
+import "./App.css";
+import "./index.css";
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<div className="flex justify-center mt-3">
+				<h1 className="inline text-5xl text-yellow-100">Block</h1>
+				<h1 className="inline text-5xl italic text-yellow-100">Net</h1>
+			</div>
+			<div className="flex justify-center mt-3">
+				<h2 className="block text-white">
+					Blockchain aimed at storing Neural Network weights and
+					hyperparameters
+				</h2>
+			</div>
+			<div className="flex justify-center mt-3">
+				<h2 className="block text-yellow-100">Coming soon...</h2>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
